630d08a87aaceb5174ba17e53a320912a2afbe82
Yura <52866390+darkne33@users.noreply.github.com>
2021-10-26 17:00:29 +0300

    Release 0.1v
    Release package

---
+++ Assets/Resources/SCData/ControlWindow.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ControlData
+{
+    public class ControlWindow : EditorWindow
+    {
+        private DataSC _dataSC;
+
+        private bool _isMonobehaviourClass;
+        private string _className;
+        private int _countFields;
+
+        private readonly List<string> _nameFields = new List<string>();
+        
+        private int[] _typeFields = new int[99];
+        private string[] _typeFieldsText = new string[99];
+        
+        private int _selectedField;
+        private string[] _typeFieldOptions = {"int", "float", "string", "Vector3", "Quaternion", "Vector2"};
+
+
+        public ControlWindow()
+        {
+            _dataSC = new DataSC();
+        }
+
+        [MenuItem("Serializable Data/Create Data")]
+        private static void Initialize() 
+            => GetWindow(typeof(ControlWindow), true, "Create Data");
+
+
+        private void OnGUI()
+        {
+            _isMonobehaviourClass = EditorGUILayout.Toggle("Monobehaviour class", _isMonobehaviourClass);
+            
+            
+            InitClassName();
+            
+            InitFields();
+
+            ResetFieldsButton();
+            CreateSCButton();
+        }
+        
+        
+        private void InitClassName()
+            =>_className = EditorGUILayout.TextField("SC name", _className);
+
+
+        private void InitFields()
+        {
+            GUILabelFields();
+            
+            _countFields = EditorGUILayout.IntField("Count fields in data", _countFields);
+            
+            for (int i = 0; i < _countFields; i++)
+            {
+                _nameFields.Add("NameField");
+                _nameFields[i] = EditorGUILayout.TextField("Name field", _nameFields[i]);
+                _typeFields[i] = EditorGUILayout.Popup("TypeField", _typeFields[i], _typeFieldOptions);
+                switch (_typeFields[i])
+                {
+                    case 0:
+                        _typeFieldsText[i] = "int";
+                        break;
+                    case 1:
+                        _typeFieldsText[i] = "float";
+                        break;
+                    case 2:
+                        _typeFieldsText[i] = "string";
+                        break;
+                    case 3:
+                        _typeFieldsText[i] = "Vector3";
+                        break;
+                    case 4:
+                        _typeFieldsText[i] = "Quaternion";
+                        break;
+                    case 5:
+                        _typeFieldsText[i] = "Vector2";
+                        break;
+                }
+            }
+        }
+        
+
+        private void GUILabelFields()
+        {
+            GUIStyle guiStyle = new GUIStyle();
+            guiStyle.alignment = TextAnchor.MiddleCenter;
+            guiStyle.normal.textColor = Color.white;
+            EditorGUILayout.LabelField("Fields",guiStyle);
+        }
+        
+
+        private void ResetFieldsButton()
+        {
+            if (GUILayout.Button("Reset fields"))
+                _nameFields.Clear();
+        }
+
+        private void CreateSCButton()
+        {
+            if (GUILayout.Button("Create SC script"))
+                _dataSC.Create(_className, _countFields, _nameFields, _typeFieldsText, _isMonobehaviourClass);
+        }
+    }
+}
---
+++ Assets/Resources/SCData/DataSC.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+namespace ControlData
+{
+    public class DataSC
+    {
+        public void Create(string className, int countFields, List<string> nameFields, string[] typeFieldText, bool isMonobehaviourClass)
+        {
+            string classDefinition = String.Empty;
+            string monobehaviourDefinition = isMonobehaviourClass ? " : MonoBehaviour" : "";
+            string serializeKeyDefinition = isMonobehaviourClass ? "[SerializeField] " : "";
+            
+            classDefinition += "using System;" + Environment.NewLine;
+            classDefinition += "using System.IO;" + Environment.NewLine;
+            classDefinition += "using UnityEngine;" + Environment.NewLine;
+        
+            classDefinition += Environment.NewLine;
+        
+            classDefinition += "[System.Serializable]" + Environment.NewLine;
+            classDefinition += "public class " + className + monobehaviourDefinition + Environment.NewLine;
+            classDefinition += "{" + Environment.NewLine;
+
+            classDefinition += "    " + serializeKeyDefinition + "private string _key;" + Environment.NewLine;
+            classDefinition += "    private string _jsonData;" + Environment.NewLine;
+
+            for (int i = 0; i < countFields; i++)
+                classDefinition += "    public " + typeFieldText[i] + " "  + nameFields[i] + ";" + Environment.NewLine;
+
+            classDefinition += Environment.NewLine;
+
+            classDefinition += "    public void SaveData()" + Environment.NewLine;
+            classDefinition += "    {";
+
+            classDefinition += Environment.NewLine;
+
+            classDefinition +=
+                "        File.WriteAllText(Application.persistentDataPath + \"/\" + GetType().Name, JsonUtility.ToJson(this));" + Environment.NewLine;
+            classDefinition += "    }";
+
+            classDefinition += Environment.NewLine;
+            classDefinition += Environment.NewLine;
+        
+            classDefinition += "    public void LoadData()" + Environment.NewLine;
+            classDefinition += "    {" + Environment.NewLine;
+            classDefinition += "        string loadJsonData = File.ReadAllText(Application.persistentDataPath + \"/\" + GetType().Name);" + Environment.NewLine;
+            classDefinition += "        JsonUtility.FromJsonOverwrite(loadJsonData, this);" + Environment.NewLine;
+            classDefinition += "    }" + Environment.NewLine;
+            
+            classDefinition += "}";
+            File.WriteAllText("Assets/Resources/" + className + ".cs", classDefinition);
+
+            EditorUtility.RevealInFinder("Assets/Resources");
+        }
+    }
+}
